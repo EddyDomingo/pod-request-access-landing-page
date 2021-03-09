24b959a274bdefc3e42f4856b807ba5dd96bc9af
EddyDomingo <68975969+EddyDomingo@users.noreply.github.com>
2021-03-09 17:50:00 -0600

    final commit

--- starter-code/scripts.js
+++ starter-code/scripts.js
@@ -1,1 +1,24 @@
 console.log('Hello World');
+
+
+document.getElementById('emailButton').addEventListener("click", validateForm);
+
+
+function validateForm(){
+    let input = document.getElementById('inputField').value;
+    const mailFormat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (input == ""){
+        alert("Oops! Please add your email");
+    }else{
+        if ( input.match(mailFormat) )
+        {
+          alert("Request submitted!")
+          return (true)
+        }
+          alert("Oops! Please check your email")
+          return (false)
+      }
+    
+    input = '';
+    }
